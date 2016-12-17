59cc05015e7fda1537bd410462b749bf3c26e449
marco <s105062638@m105.nthu.edu.tw>
2016-12-17 16:46:39 +0800

    remove white space

--- word.py
+++ word.py
@@ -1,25 +1,39 @@
 import json
+
+def generateWords(stu, final):
+    for i in range(0, len(stu)):
+        wordlist = []
+        for v in stu[i]['vocabularyList']:
+            w = v['word']
+            # remove white space of the word
+            w = w.strip()
+            if w in wordlist:
+                # duplicated vocabulary
+                continue
+            else:
+                wordlist.append(w)
+        user = {
+            'memberId' : stu[i]['memberId'],
+            'wordList' : wordlist
+        }
+        final.append(user)
+
 openfile = open('studentBehaviorInfo_1.json')
 writefile = open('dictionary.json', 'w')
-stu = json.load(openfile)
 
+students = json.load(openfile)
 final = []
-for i in range(0, len(stu)):
-    wordlist = []
-    for v in stu[i]['vocabularyList']:
-        w = v['word']
-        if(w in wordlist):
-            # duplicated vocabulary
-            continue
-        else:
-            wordlist.append(w)
-    user = {
-        'memberId' : stu[i]['memberId'],
-        'wordList' : wordlist
-    }
-    final.append(user)
+generateWords(students, final)
+openfile.close()
+
+openfile = open('studentBehaviorInfo_2.json')
+students = json.load(openfile)
+generateWords(students, final)
+openfile.close()
+
 # print final
 json.dump(final, writefile)
-openfile.close()
 writefile.close()
+
+
     
